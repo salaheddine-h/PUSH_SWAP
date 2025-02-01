7ab82eee8ab496499e8688e10b3247990803996d
Salah Eddine Hali <salhali@e2r4p3.1337.ma>
2025-02-01 21:36:06 +0100

    Done

--- source/puch_swap.c
+++ source/puch_swap.c
@@ -6,76 +6,28 @@
 /*   By: salhali <salhali@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 15:02:45 by salhali           #+#    #+#             */
-/*   Updated: 2025/01/31 22:30:21 by salhali          ###   ########.fr       */
+/*   Updated: 2025/02/01 21:21:32 by salhali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// // #include "push_swap.h"
+// #include "push_swap.h"
+
+// #include "../libft/libft.a"
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <limits.h>
 
-#  define INT_MIN	(-INT_MAX - 1)
-#  define INT_MAX	2147483647
-
-// int	ft_isdigit(int arg)
-// {
-// 	if (arg >= '0' && arg <= '9')
-// 		return (1);
-// 	else
-// 		return (0);
-// }
-// int is_valid_number(char *str) //parsing dyal input
-// {
-//     int i;
-//     long num;
-
-//     i = 0;
-//     if (!str || str[0] == '\0')
-//         return (0);
-//     if (str[i] == '-' || str[i] == '+')
-//         i++;
-//     while (str[i])
-//     {
-//         if (!ft_isdigit(str[i]))
-//             return (0);
-//         i++;
-//     }
-//     num = atol(str);
-//     if(num < INT_MIN || num > INT_MAX)
-//         return(0);
-//     return (1);
-// }
-int	main(int argc, char **argv)
-{
-    t_stack stackB;
-    int check;
-    long    num;
-
-    stackB = = {NULL}; // Initialize stack B as empty Hitach stack B haso ikon khawi
-    if(argc > 1)
-    {
-        check = is_valid_number(argv[1]); // Correction ici, 3adi ndir argv[1]
-        if (check)
-        {
-            num = atol(argv[1]);
-            push(&stackB, num);
-            write(1, "Valid number\n", 13);
-        }
-        else
-        {
-                write(1, "Invalid number\n", 15);
-        }
-        return (0);    
-    }
-    else
-        write(1, "Kol m3ahom ou 7di mnhom\n", 25);
-        write(1, "T3am kayn ou lghdar kayn\n", 26);
-    return(0);
-}
-
+// # define INT_MIN    (-INT_MAX - 1)
+// # define INT_MAX    2147483647
 
 // Function to check if character is digit
+typedef struct s_node
+{
+    long data;
+    struct s_node *next;
+} t_node;
+
 int ft_isdigit(int arg)
 {
     if (arg >= '0' && arg <= '9')
@@ -83,7 +35,6 @@ int ft_isdigit(int arg)
     else
         return (0);
 }
-
 // Check if string is a valid number
 int is_valid_number(char *str)
 {
@@ -102,13 +53,12 @@ int is_valid_number(char *str)
         i++;
     }
     num = atol(str);
-    if(num < INT_MIN || num > INT_MAX)
+    if(num < INT_MIN || num > INT_MAX) //check is number kbiir mn range (int)
         return(0);
     return (1);
 }
-
 // Function to create a new node in the stack
-t_node *new_node(long data)
+t_node *new_node(long data) //// Kat créer node jdida
 {
     t_node *node = (t_node *)malloc(sizeof(t_node));
     if (!node)
@@ -118,46 +68,73 @@ t_node *new_node(long data)
     return node;
 }
 
-// Push function to add element to the stack
-void push(t_stack *stack, long data)
+t_node *get_last_node(t_node **stack)
 {
-    t_node *node = new_node(data);
-    if (node)
+    t_node *tmp;
+
+    if (stack)
+        return NULL;
+    tmp = *stack;
+    while (tmp->next)
     {
-        node->next = stack->top;
-        stack->top = node;
+        tmp = tmp->next;
+    }
+    return(tmp);
+}
+// Push function to add element to the stack
+void push(t_node **stack, long data) //Katzid node jdida f stack:
+{
+    t_node *node = new_node(data); 
+    t_node *tmp;
+    tmp = get_last_node(stack);
+    if (tmp)
+        tmp->next = node;
+    *stack = node;
+}
+
+void print_stack(t_node **stack)
+{
+    t_node *tmp;
+
+    if(!stack)
+        return ;
+    tmp = *stack;
+    while (tmp)
+    {
+        printf("-- %ld -- \n",tmp->data);
+        tmp = tmp->next;
     }
 }
 
-// Function to check and push valid number from argv[1] to stack B
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    t_stack stackB = {NULL};  // Initialize stack B as empty
-    // t_node  stackA
-    int check;
+    t_node *stackB;
+    int     check;
+    long    num;
+    int i;
 
-    if (argc > 1)
+    stackB = NULL; // Initialize stack B as empty Hitach stack B haso ikon khawi
+    if(argc > 1)
     {
-        check = is_valid_number(argv[1]);  // Check if argv[1] is a valid number
-        
-        if (check)
+        check = is_valid_number(argv[1]); // Correction ici, 3adi ndir argv[1]
+        while(check)
         {
-            // If valid, push the number to stack B
-            long num = atol(argv[1]);
-            push(&stackB, num);
-            write(1, "Number pushed to stack B\n", 25);
+            if (check)
+            {
+                num = atol(argv[1]);
+                push(&stackB, num);
+                write(1, "stackB siftna lih Data\n", 24);
+            }
+            else
+            {
+                    write(1, "Invalid number\n", 15);
+            }
+            print_stack(&stackB);
+            return (0);
         }
-        else
-        {
-            write(1, "Invalid number\n", 15);
-        }
-
-        return (0);
     }
     else
-    {
         write(1, "Kol m3ahom ou 7di mnhom\n", 25);
         write(1, "T3am kayn ou lghdar kayn\n", 26);
-    }
-    return (0);
+    return(0);
 }
--- source/puch_swap.h
+++ source/puch_swap.h
@@ -6,22 +6,27 @@
 /*   By: salhali <salhali@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 20:27:52 by salhali           #+#    #+#             */
-/*   Updated: 2025/01/31 22:31:38 by salhali          ###   ########.fr       */
+/*   Updated: 2025/02/01 21:03:28 by salhali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-#  define INT_MIN	(-INT_MAX - 1)
-#  define INT_MAX	2147483647
+# define INT_MIN    (-INT_MAX - 1)
+# define INT_MAX    2147483647
 
+#include "../libft/libft.a"
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-
+//----- Part one -------// 
 
 int is_valid_number(char *str);
+int ft_isdigit(int arg);
+void print_stack(t_node **stack);
+void push(t_node **stack, long data);
+t_node *get_last_node(t_node **stack);
 
 
 typedef struct s_node
@@ -29,10 +34,5 @@ typedef struct s_node
     long data;
     struct s_node *next;
 } t_node;
-
-typedef struct s_stack
-{
-    t_node *top;
-} t_stack;
 
 #endif
