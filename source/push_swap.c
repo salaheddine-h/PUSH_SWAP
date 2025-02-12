1f3de9e93431f776851191a7600cbe97e7b8f4d6
salaheddine <salaheddineh7@gmail.com>
2025-02-12 10:14:27 +0100

    Done-------

--- source/push_swap.c
+++ source/push_swap.c
@@ -3,76 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: salhali <salhali@student.42.fr>            +#+  +:+       +#+        */
+/*   By: salah <salah@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 15:02:45 by salhali           #+#    #+#             */
-/*   Updated: 2025/02/11 19:25:21 by salhali          ###   ########.fr       */
+/*   Updated: 2025/02/12 10:09:19 by salah            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-// int is_valid_number(char *str)
-// {
-//     int i;
-//     long num;
-
-//     i = 0;
-//     if (!str || str[0] == '\0')
-//         return (0);
-//     if (str[i] == '-' || str[i] == '+')
-//         i++;
-//     num = atol(str);
-//     if(num < INT_MIN || num > INT_MAX) //check is number kbiir mn range (int)
-//         return(0);
-//     while (str[i])
-//     {
-//         if (!ft_isdigit(str[i]))
-//             return (0);
-//         i++;
-//     }
-//     // Check duplicate
-//     while (str[i])
-//     {
-//         if (str[i]) // L9ina duplicate
-//             return (0);
-//     }
-//     return(1);
-// }
-// int is_valid_number(char *str) {
-//     int i = 0;
-//     if (str[i] == '-' || str[i] == '+')  // Allow negative & positive signs
-//         i++;
-//     while (str[i]) {
-//         if (!isdigit(str[i]))  // Check wach ghir numbers
-//             return (0);
-//         i++;
-//     }
-//     return (1);
-// }
-
 void    check_dup(t_node **stack, long num)
 {
-
     t_node *tmp;
 
-    if (!stack || !*stack)  // ✅ Check if stack exists
+    if (!stack || !*stack)  //Check if stack exists
     {
         return;
     }
-
     tmp = *stack;
-    while (tmp)  // ✅ Loop while `tmp` is not NULL
+    while (tmp)  //Loop while `tmp` is not NULL
     {
         if (tmp->data == num)
         {
             // print_stack(stack);
             printf("DUPLICATED NUMBER");
-            exit(1);// ✅ Safe to access `data`
+            exit(1);//Safe to access `data`
         }
         tmp = tmp->next;
     }
-    // return(1);                
 }
 
 int is_valid_number(char *str, t_node *stack)
@@ -95,49 +53,33 @@ int is_valid_number(char *str, t_node *stack)
             return (0);
         i++;
     }
-    // printf("Hna mochkiL");
-
-        // print_stack(&stack);
-    //Check Duplicate in stackA
-    // temp = stackA;
-    // while (temp)
-    // {
-    //     // printf("->%ld\n",temp->data); //just check
-    //     if (temp->data == num) // L9ina deja kayn f stackA
-    //         return (0);
-    //     temp = temp->next;
-    // }
     return (1);
 }
 
 int main(int argc, char **argv)
 {
-    t_node *stackA = NULL;  // ✅ Initialize stackA
+    t_node *stackA;
     int num;
     int i;
 
+    stackA= NULL;
     if (argc > 1)
     {
         i = 1;
-        while (i < argc)  // ✅ Loop through arguments
+        while (i < argc)  //Loop through arguments
         {
             num = atol(argv[i]);
-            printf("%ld", num);  // ✅ Convert string to long first
-
-            if (!is_valid_number(argv[i], stackA))  // ✅ Validate input number
-            {
-                write(2, "Error\n", 6);
-                return (1);
-            }
+            if (!is_valid_number(argv[i], stackA))  //Validate input number
+                return (write(2, "Error\n", 6),1);
             check_dup(&stackA, num);
-            push(&stackA, num);  // ✅ Push number to stack
+            push(&stackA, num);  //Push number to stack
             i++;
         }
-        print_stack(&stackA);  // ✅ Debugging: print stack
+        print_stack(&stackA);  //Debugging: print stack
     }
     else
     {
-        write(2, "Error\n", 6);  // ✅ Proper error message
+        write(2, "Error, Bad Argmu\n", 18);  //Proper error message
         return (1);
     }
     return (0);
--- source/push_swap.h
+++ source/push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: salhali <salhali@student.42.fr>            +#+  +:+       +#+        */
+/*   By: salah <salah@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 20:27:52 by salhali           #+#    #+#             */
-/*   Updated: 2025/02/11 19:12:09 by salhali          ###   ########.fr       */
+/*   Updated: 2025/02/12 10:09:52 by salah            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,19 @@
 // # define INT_MIN    (-INT_MAX - 1)
 // # define INT_MAX    2147483647
 
-#include "../libft/libft.h"
+// #include "../libft/libft.h"
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <limits.h>
 
-//----- Part one -------// 
+//----- Part one -------//
 typedef struct s_node
 {
     long data; // Node-->Data
     struct s_node *next;// Node-->*Next
 } t_node;
 
-int	ft_isdigit(int arg);
 void    check_dup(t_node **stack, long num);
 int is_valid_number(char *str,t_node *stack); // Check if string is a valid number
 int ft_isdigit(int arg); //Funciton check is digite
