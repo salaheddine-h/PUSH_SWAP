75ec962062f3b7bf104bca84058e357220f4edc9
salaheddine <salaheddineh7@gmail.com>
2025-02-16 13:15:37 +0100

    it's, Done

--- source/push_swap.c
+++ source/push_swap.c
@@ -6,15 +6,13 @@
 /*   By: salah <salah@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 15:16:17 by salah             #+#    #+#             */
-/*   Updated: 2025/02/15 15:28:29 by salah            ###   ########.fr       */
+/*   Updated: 2025/02/15 15:50:17 by salah            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
 void  redix_sort(t_node **stackA, t_node **stackB)
-{
-  t_node  
-}
+
 
 
